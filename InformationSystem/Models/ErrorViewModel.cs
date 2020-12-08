e6ece7a5a07b62dc77f18edc0c531f2367cd4ec2
Вадим Булат <justliveinlife@gmail.com>
2020-12-08 15:42:20 +0300

    Add project files.

---
+++ InformationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InformationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
